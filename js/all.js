3df76ed19fd376d4546ff81e7fe5866736744f0b
Lodibet7 <131884030+Lodibet7@users.noreply.github.com>
2023-09-14 15:18:18 +0800

    Create all.js

---
+++ js/all.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('.footer-lang').click(function (e) {
+    $('.dropdown-toggle').toggleClass('active');
+    $('.dropdown-menu').toggleClass('active');
+  });
+});
